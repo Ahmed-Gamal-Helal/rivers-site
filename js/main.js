526668ab4e26173c2254aa08674b6fa420961327
Jemy1234 <ahmdtawfiek@gmail.com>
2018-01-17 21:49:59 +0200

    Rivers

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(function(){
     'use strict';
-    $("nav-tabs ul li a, ul.navbar-nav li").click(function (){
+    $("nav-tabs ul li, ul.navbar-nav li").click(function (){
         $(this).addClass('active').siblings().removeClass('active');
         // $(this).attr('color', '#F00 !important');
     });
-    
 
+
+    $("nav .container .navbar-collapse ul li a").on('click', function(event) {
+       if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         event.preventDefault();
+         var hash = this.hash;
+
+         $('html, body').animate({
+           scrollTop: $(hash).offset().top
+         }, 900, function(){
+           window.location.hash = hash;
+            });
+        }
+    });     
 });
 // AOS Set up
 AOS.init({
